7aae1476ea51f8a2091065f3da6f9f10df5f8d79
chengkui <chengkui@quicloud.cn>
2017-07-07 13:29:54 +0800

    冒泡排序

---
+++ src/priv/chengkui/algorithm/Bubble.java
@@ -0,0 +1,33 @@
+package priv.chengkui.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ *
+ * @author chengkui
+ * @create 2017-07-07-13:17
+ */
+public class Bubble {
+
+    public static void main(String[] args){
+        int[] arr = new int[]{2,5,7,1,6};
+        System.out.print(Arrays.toString(sort(arr)));
+    }
+
+    public static int[] sort(int[] arr){
+        int N = arr.length;
+
+        for(int i = 1; i < N; i++){
+            for(int j = i - 1; j < N - i; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
